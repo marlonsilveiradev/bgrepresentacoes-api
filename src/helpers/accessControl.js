d4fc2c991883e2dc457a1e4cd35d0fdaf84df219
Marlon <marlonsilveira.dev@gmail.com>
2026-03-31 21:16:18 -0300

    fix: enforce access control at query level in getClientById

--- src/helpers/accessControl.js
+++ src/helpers/accessControl.js
@@ -1,20 +1,33 @@
 const { ROLES } = require('../constants/roles');
 
-function buildClientAccessFilter(user) {
+function buildClientAccessFilter(user, extraFilters = {}) {
   if (!user) {
     return { id: null };
   }
 
+  let baseFilter = {};
+
   switch (user.role) {
     case ROLES.ADMIN:
-      return {};
+      baseFilter = {};
+      break;
 
     case ROLES.PARTNER:
-      return { userId: user.id };
+      baseFilter = { partner_id: user.id };
+      break;
+
+    case ROLES.USER:
+      baseFilter = { created_by: user.id };
+      break;
 
     default:
       return { id: null };
   }
+
+  return {
+    ...baseFilter,
+    ...extraFilters,
+  };
 }
 
 module.exports = {
